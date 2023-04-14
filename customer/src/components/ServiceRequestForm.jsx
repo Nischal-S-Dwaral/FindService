08e09544a7688f5d4d8c9ab2e52a51050130d14e
Nischal-S-Dwaral <nischal.dwaral@gmail.com>
2023-04-14 17:50:01 +0100

    Integrated API for the customers, creating a service request view

--- customer/src/components/ServiceRequestForm.jsx
+++ customer/src/components/ServiceRequestForm.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from "styled-components";
 import {mobile} from "../responsive";
+import {categoryValueToText} from "../data";
+import axios from "axios";
+import {useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
 
 const Container = styled.div `
   background-color: rgba(0,0,0,0.3);
@@ -102,9 +106,56 @@ const LabelWithInput = styled.div `
  * @returns {JSX.Element|null} - Modal component which contains the form for creating the service request
  */
 const ServiceRequestForm = ({ open, onClose, data }) => {
+
+    const [date, setDate] = useState("");
+    const [time, setTime] = useState("");
+    const [address, setAddress] = useState("");
+    const [description, setDescription] = useState("");
+    let categoryText = categoryValueToText.get(data.category)
+
+    const user = useSelector((state) => state.user.currentUser);
+    const navigate = useNavigate();
+
+    const handleCreateButtonClick = async (event) => {
+        event.preventDefault(); // prevents the refresh of the page
+        try {
+            let postData = JSON.stringify({
+                "serviceId": data.id,
+                "customerId": user.uid,
+                "customerName": user.username,
+                "description": description,
+                "date": date,
+                "time": time,
+                "address": address
+            });
+
+            let config = {
+                method: 'post',
+                maxBodyLength: Infinity,
+                url: 'http://localhost:8080/api/serviceRequest/add',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data : postData
+            };
+
+            const response = await axios.request(config)
+            console.log(response.data);
+
+            if (response.data.returnCode === "0") {
+                navigate("/requests", { replace: true });
+            } else {
+                console.log(response.data);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     if (!open) {
         return null;
     }
+
     return (
         <Container onClick={onClose}>
             <Wrapper onClick={(e) => {
@@ -117,32 +168,42 @@ const ServiceRequestForm = ({ open, onClose, data }) => {
                 <Form id="serviceRequestForm">
                     <LabelWithInput>
                         <Label htmlFor="serviceName">Service Name</Label>
-                        <Input id="serviceName" type="text" value={data.serviceName} disabled={true}/>
+                        <Input id="serviceName" type="text" value={data.name} disabled={true}/>
                     </LabelWithInput>
                     <LabelWithInput>
                         <Label htmlFor="category">Category</Label>
-                        <Input id="category" type="text" value="Electrical Repairs" disabled={true}/>
+                        <Input id="category" type="text" value={categoryText} disabled={true}/>
                     </LabelWithInput>
                     <LabelWithInput>
                         <Label htmlFor="requestDate">Select Date</Label>
-                        <Input id="requestDate" type="date" min={new Date().toISOString().slice(0, 10)}/>
+                        <Input id="requestDate" type="date"
+                               min={new Date().toISOString().slice(0, 10)}
+                               onChange={(e)=> setDate(e.target.value)}
+                        />
                     </LabelWithInput>
                     <LabelWithInput>
                         <Label htmlFor="requestTime">Select Time</Label>
-                        <Input id="requestTime" type="time"/>
+                        <Input id="requestTime" type="time"
+                               onChange={(e)=> setTime(e.target.value)}
+                        />
                     </LabelWithInput>
                     <LabelWithInput>
                         <Label htmlFor="address">Address</Label>
-                        <Input id="address" type="text" placeholder="Please provide complete address"/>
+                        <Input id="address" type="text"
+                               placeholder="Please provide complete address"
+                               onChange={(e)=> setAddress(e.target.value)}
+                        />
                     </LabelWithInput>
                     <LabelWithInput>
                         <Label htmlFor="description">Description</Label>
                         <TextArea
                             id="description" cols={40} rows={10}
                             name="descriptionText" form="serviceRequestForm"
-                            placeholder="Please provide clear description..."/>
+                            placeholder="Please provide clear description..."
+                            onChange={(e)=> setDescription(e.target.value)}
+                        />
                     </LabelWithInput>
-                    <Button>CREATE</Button>
+                    <Button onClick={handleCreateButtonClick}>CREATE</Button>
                 </Form>
             </Wrapper>
         </Container>
--- customer/src/data.js
+++ customer/src/data.js
@@ -46,6 +46,16 @@ export const categories = [
     }
 ]
 
+export const categoryValueToText = new Map([
+    ["cleaning", "Cleaning"],
+    ["babySitting", "Baby Sitting"],
+    ["pestControl", "Pest Control"],
+    ["others", "Others"],
+    ["plumbing", "Plumbing"],
+    ["electricalRepairs", "Electrical Repairs"],
+    ["beauty", "Beauty"]
+]);
+
 export const serviceRequests = [
     {
         id: 1,
@@ -112,205 +122,6 @@ export const serviceRequests = [
         serviceName: "Bathroom Cleaning",
         serviceProvider: "Urban Clean",
         status: "Pending"
-    }
-];
-
-export const serviceDetails = [
-    {
-        id: 1,
-        serviceName: "Portswood Electronics",
-        location: "Portswood, Southampton",
-        timings: "Mon-Fri 9am-5pm",
-        price: "800",
-        rating: 2.5,
-        img: "https://media.istockphoto.com/id/1409304338/photo/electricity-and-electrical-maintenance-service-engineer-hand-holding-ac-multimeter-checking.jpg?s=612x612&w=0&k=20&c=Pj3gqobTp67aPN8l-lm6q-uT2DDnm-aeFk7Gkvmbaok=",
-        description: "Wanting to fix a faulty electrical flow? Or looking forward to installing new electrical features at home? We have all the expertise required on the electrical front.",
-        reviews: [
-            {
-                id: 1,
-                username: "Nischal",
-                rating: 4.5,
-                date: "Mar, 2023",
-                comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-            },
-            {
-                id: 2,
-                username: "John",
-                rating: 3.8,
-                date: "Dec, 2022",
-                comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor teur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-            },
-            {
-                id: 3,
-                username: "Nick Long Name Random",
-                rating: 4.8,
-                date: "Jun, 2022",
-                comment: "Excellent Service!!"
-            },
-            {
-                id: 4,
-                username: "Karen",
-                rating: 1,
-                date: "Feb, 2022",
-                comment: "Very poor!! Very disappointing!! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
-            }
-        ],
-        photos: [
-            {
-                id: 1,
-                img: "https://media.istockphoto.com/id/1409304338/photo/electricity-and-electrical-maintenance-service-engineer-hand-holding-ac-multimeter-checking.jpg?s=612x612&w=0&k=20&c=Pj3gqobTp67aPN8l-lm6q-uT2DDnm-aeFk7Gkvmbaok="
-            },
-            {
-                id: 2,
-                img: "https://images.unsplash.com/photo-1657664066042-c59e5f84b7a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZWxlY3RyaWNpYW58ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-            },
-            {
-                id: 3,
-                img: "https://images.unsplash.com/photo-1621905251189-08b45d6a269e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZWxlY3RyaWNpYW58ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-            },
-            {
-                id: 4,
-                img: "https://plus.unsplash.com/premium_photo-1663100935279-660c42ef1d41?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzd8fGVsZWN0cmljaWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-            },
-            {
-                id: 5,
-                img: "https://images.unsplash.com/photo-1641194298727-fa6e5401bf1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fGVsZWN0cmljaWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-            }
-        ]
-    },
-    {
-        id: 2,
-        serviceName: "John Plumbing",
-        location: "Shirley, Southampton",
-        timings: "Mon-Thu 11am-9pm",
-        price: "400",
-        rating: 3.75,
-        img: "https://plus.unsplash.com/premium_photo-1663047166207-fd717b9a0ba7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHBsdW1iZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-        description: "A leaking pipe flooding your home? Or looking forward to installing new plumbing fixtures at home? Then John Plumbing is the right person to call.",
-        reviews: [
-            {
-                id: 1,
-                username: "Nischal",
-                rating: 4.5,
-                date: "Mar, 2023",
-                comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-            },
-            {
-                id: 2,
-                username: "John",
-                rating: 3.8,
-                date: "Dec, 2022",
-                comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor teur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-            },
-            {
-                id: 3,
-                username: "Nick",
-                rating: 4.8,
-                date: "Jun, 2022",
-                comment: "Excellent Service!!"
-            }
-        ],
-        photos: [
-            {
-                id: 1,
-                img: "https://media.istockphoto.com/id/1409304338/photo/electricity-and-electrical-maintenance-service-engineer-hand-holding-ac-multimeter-checking.jpg?s=612x612&w=0&k=20&c=Pj3gqobTp67aPN8l-lm6q-uT2DDnm-aeFk7Gkvmbaok="
-            },
-            {
-                id: 2,
-                img: "https://images.unsplash.com/photo-1657664066042-c59e5f84b7a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZWxlY3RyaWNpYW58ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-            },
-            {
-                id: 3,
-                img: "https://images.unsplash.com/photo-1621905251189-08b45d6a269e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZWxlY3RyaWNpYW58ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-            },
-            {
-                id: 4,
-                img: "https://plus.unsplash.com/premium_photo-1663100935279-660c42ef1d41?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzd8fGVsZWN0cmljaWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-            }
-        ]
-    },
-    {
-        id: 3,
-        serviceName: "Face Stories",
-        location: "Chelsea, London",
-        timings: "Tue, Wed and Fri 9am-5pm",
-        price: "1500",
-        rating: 4.8,
-        img: "https://media.istockphoto.com/id/528908438/photo/wedding-makeup-artist-making-a-make-up-for-bride.jpg?s=612x612&w=0&k=20&c=KsOMyM9A3v0JmRtEnk3b4KumuXyEZAGjM4Yfij4YL94=",
-        description: "Only a professional artist can understand the mood and style of make-up to make you look astonishing.",
-        reviews: [
-            {
-                id: 1,
-                username: "Nischal",
-                rating: 4.5,
-                date: "Mar, 2023",
-                comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-            },
-            {
-                id: 2,
-                username: "John",
-                rating: 3.8,
-                date: "Dec, 2022",
-                comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor teur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-            },
-            {
-                id: 3,
-                username: "Nick",
-                rating: 4.8,
-                date: "Jun, 2022",
-                comment: "Excellent Service!!"
-            },
-            {
-                id: 4,
-                username: "Karen",
-                rating: 1,
-                date: "Feb, 2022",
-                comment: "Very poor!! Very disappointing!! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-            }
-        ],
-        photos: [
-            {
-                id: 1,
-                img: "https://media.istockphoto.com/id/1409304338/photo/electricity-and-electrical-maintenance-service-engineer-hand-holding-ac-multimeter-checking.jpg?s=612x612&w=0&k=20&c=Pj3gqobTp67aPN8l-lm6q-uT2DDnm-aeFk7Gkvmbaok="
-            },
-            {
-                id: 2,
-                img: "https://images.unsplash.com/photo-1657664066042-c59e5f84b7a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZWxlY3RyaWNpYW58ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-            },
-            {
-                id: 3,
-                img: "https://images.unsplash.com/photo-1621905251189-08b45d6a269e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZWxlY3RyaWNpYW58ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-            }
-        ]
-    },
-    {
-        id: 4,
-        serviceName: "Kidz Company",
-        location: "Manchester",
-        timings: "Mon-Sat 7am-5pm",
-        price: "750",
-        rating: 3.5,
-        img: "https://images.unsplash.com/photo-1526634332515-d56c5fd16991?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJhYnklMjBzaXR0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-        description: "If you are looking for the best baby sitters, you must consider the Kidz Company.",
-        reviews: [
-            {
-                id: 1,
-                username: "Nischal",
-                rating: 4.5,
-                date: "Mar, 2023",
-                comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-            }
-        ],
-        photos: [
-            {
-                id: 1,
-                img: "https://media.istockphoto.com/id/1409304338/photo/electricity-and-electrical-maintenance-service-engineer-hand-holding-ac-multimeter-checking.jpg?s=612x612&w=0&k=20&c=Pj3gqobTp67aPN8l-lm6q-uT2DDnm-aeFk7Gkvmbaok="
-            },
-            {
-                id: 2,
-                img: "https://images.unsplash.com/photo-1657664066042-c59e5f84b7a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZWxlY3RyaWNpYW58ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-            }
-        ]
     }
 ];
 
